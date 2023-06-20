e67f34f0f4e200b8d69370afc876e0d91b1d36a0
Adriana Treviño <70771428+adrianatrevinoe@users.noreply.github.com>
2023-06-20 13:57:23 +0000

    added patient view

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import patient
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('patient/', patient),
 ]
