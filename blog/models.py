f334c88e170c8d9d7047be744b8b696cc6275148
Adriana Treviño <70771428+adrianatrevinoe@users.noreply.github.com>
2023-06-20 14:32:54 +0000

    added nulls and migration

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,12 @@ from django.db import models
 from django.utils import timezone
 
 class Doctor(models.Model):
-    name = models.CharField(max_length=150)
-    identification_card = models.IntegerField(max_length=200)
-    specialty = models.CharField(max_length=150)
-    phone = models.IntegerField()
-    location = models.TextField()
-    bio = models.TextField()
+    name = models.CharField(max_length=150,null=True)
+    identification_card = models.IntegerField(null=True)
+    specialty = models.CharField(max_length=150,null=True)
+    phone = models.IntegerField(null=True)
+    location = models.TextField(null=True)
+    bio = models.TextField(null=True)
     EntranceHours=(
         (
             '7:00', '7:00'
@@ -67,8 +67,8 @@ class Doctor(models.Model):
             '19:00', '19:00'
         )
     )
-    CheckIn = models.CharField(max_length=100,choices=EntranceHours)
-    CheckOut=models.CharField(max_length=100,choices=OutOfOffice)
+    #CheckIn = models.CharField(max_length=100,choices=EntranceHours)
+    #CheckOut=models.CharField(max_length=100,choices=OutOfOffice)
 
 
 
@@ -98,17 +98,17 @@ class Patient(models.Model):
             'Yes'
         )
     )
-    name = models.CharField(max_length=150)
-    age = models.IntegerField()
-    gender = models.CharField(max_length=100,choices=genderOptions)
-    place_of_birth = models.CharField(max_length=100)
-    height = models.IntegerField()
-    weight = models.IntegerField()
-    mail = models.CharField(max_length = 100)
-    phone = models.IntegerField()
-    last_visit = models.TextField()
-    alcohol = models.CharField(max_length=100, choices=yes_no)
-    smoke = models.CharField(max_length=100, choices=yes_no)
-    drugs = models.CharField(max_length=100, choices=yes_no)
-    religion = models.CharField(max_length=255)
-    allergies = models.CharField(max_length=100, choices=yes_no)
+    name = models.CharField(max_length=150,null=True)
+    age = models.IntegerField(null=True)
+    gender = models.CharField(max_length=100,choices=genderOptions,null=True)
+    place_of_birth = models.CharField(max_length=100,null=True)
+    height = models.IntegerField(null=True)
+    weight = models.IntegerField(null=True)
+    mail = models.CharField(max_length = 100,null=True)
+    phone = models.IntegerField(null=True)
+    last_visit = models.TextField(null=True)
+    alcohol = models.CharField(max_length=100, choices=yes_no,null=True)
+    smoke = models.CharField(max_length=100, choices=yes_no,null=True)
+    drugs = models.CharField(max_length=100, choices=yes_no,null=True)
+    religion = models.CharField(max_length=255,null=True)
+    allergies = models.CharField(max_length=100, choices=yes_no,null=True)
